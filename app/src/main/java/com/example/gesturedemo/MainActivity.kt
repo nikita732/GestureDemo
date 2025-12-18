1eace556abe21aa13b5ddbf417b9f226dab1e2a2
Start project <nik_orlov77@mail.ru>
2025-12-19 00:01:52 +0700

    DragDemo

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -5,18 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import kotlin.math.roundToInt
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    TapPressDemo(modifier)
+    DragDemo(modifier)
 }
 
 @Composable
@@ -73,10 +81,79 @@ fun TapPressDemo(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun DragDemo(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        var xOffset by remember { mutableStateOf(0f) }
+
+        Box(
+            modifier = Modifier
+                .offset { IntOffset(xOffset.roundToInt(), 0) }
+                .size(100.dp)
+                .background(Color.Blue)
+                .draggable(
+                    orientation = Orientation.Horizontal,
+                    state = rememberDraggableState { distance ->
+                        xOffset += distance
+                    }
+                )
+        )
+    }
+}
+
+@Composable
+fun MultiDragDemo(modifier: Modifier = Modifier) {
+    val boxSize = 100.dp
+    val boxSizePx = with(LocalDensity.current) { boxSize.toPx() }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        var offset by remember { mutableStateOf(Offset.Zero) }
+
+        Box(
+            modifier = Modifier
+                .size(boxSize)
+                .offset { IntOffset(offset.x.roundToInt(), offset.y.roundToInt()) }
+                .background(Color.Red)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+
+                        val newX = offset.x + dragAmount.x
+                        val newY = offset.y + dragAmount.y
+
+                        val maxX = size.width - boxSizePx
+                        val maxY = size.height - boxSizePx
+
+                        offset = Offset(
+                            x = newX.coerceIn(0f, maxX),
+                            y = newY.coerceIn(0f, maxY)
+                        )
+                    }
+                }
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun TapPressDemoPreview() {
     GestureDemoTheme {
         TapPressDemo()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DragDemoPreview() {
+    GestureDemoTheme {
+        DragDemo()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MultiDragDemoPreview() {
+    GestureDemoTheme {
+        MultiDragDemo()
     }
 }
